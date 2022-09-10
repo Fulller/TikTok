5ce16f403e1f03cfe9e24ad13824e91dadcf1623
phuocday <phuocday@gmail.com>
2022-09-10 10:16:43 +0700

    header#3

---
+++ src/component/Button/index.js
@@ -0,0 +1,49 @@
+import { Link, resolvePath } from 'react-router-dom';
+import classNames from 'classnames/bind';
+import styles from './Button.module.scss';
+let cx = classNames.bind(styles);
+function Button({ children, ...props }) {
+    let Tag = 'button';
+    let classes = ['wrapper'];
+    for (let prop in props) {
+        if (props[prop]) {
+            switch (prop) {
+                case 'to':
+                    Tag = Link;
+                    break;
+                case 'href':
+                    Tag = 'a';
+                    break;
+                case 'primary':
+                    classes.push('primary');
+                    break;
+                case 'outline':
+                    classes.push('outline');
+                    break;
+                case 'small':
+                    classes.push('small');
+                    break;
+                case 'large':
+                    classes.push('large');
+                    break;
+                case 'text':
+                    classes.push('text');
+                    break;
+                case 'disable':
+                    classes.push('disable');
+                    Object.keys(props).forEach((key) => {
+                        if (key.startsWith('on') && typeof props[key] == 'function') {
+                            delete props[key];
+                        }
+                    });
+                    break;
+            }
+        }
+    }
+    return (
+        <Tag className={cx(...classes)} {...props}>
+            {children}
+        </Tag>
+    );
+}
+export default Button;
--- src/component/Layout/component/Header/index.js
+++ src/component/Layout/component/Header/index.js
@@ -1,22 +1,57 @@
+import { useState } from 'react';
+import { Link } from 'react-router-dom';
+import React from 'react';
+import Tippy from '@tippyjs/react/headless';
+
 import classNames from 'classnames/bind';
 import style from './Header.module.scss';
+import { Wrapper as PopperWrapper } from '../../../Popper';
+import AcountItem from '../../../AcountItem';
+import Button from '../../../Button';
 let cx = classNames.bind(style);
 function Header() {
+    let [seacrchResult, setSeacrchResult] = useState([]);
     return (
         <header className={cx('wrapper')}>
             <div className={cx('inner')}>
                 <div className={cx('logo')}>
-                    <img src="https://upload.wikimedia.org/wikipedia/en/thumb/a/a9/TikTok_logo.svg/1280px-TikTok_logo.svg.png"></img>
+                    <Link to={'/home'}>
+                        <img src="https://upload.wikimedia.org/wikipedia/en/thumb/a/a9/TikTok_logo.svg/1280px-TikTok_logo.svg.png"></img>
+                    </Link>
                 </div>
-                <div className={cx('search')}>
-                    <input spellCheck="false" placeholder="Search accounts and videos"></input>
-                    <i className={cx('fa-solid fa-circle-xmark')}></i>
-                    <span></span>
-                    <button className={cx('search-btn')}>
-                        <i className={cx('fa-solid fa-magnifying-glass')}></i>
-                    </button>
+                <Tippy
+                    visible={seacrchResult.length > 0}
+                    interactive={true}
+                    render={(attrs) => (
+                        <div className={cx('search-result')} tabIndex="-1" {...attrs}>
+                            <PopperWrapper>
+                                <h4 className={cx('search-title')}>Acounts</h4>
+                                <AcountItem></AcountItem>
+                                <AcountItem></AcountItem>
+                                <AcountItem></AcountItem>
+                                <AcountItem></AcountItem>
+                            </PopperWrapper>
+                        </div>
+                    )}
+                >
+                    <div className={cx('search')}>
+                        <input spellCheck="false" placeholder="Search accounts and videos"></input>
+                        <i className={cx('fa-solid fa-circle-xmark')}></i>
+                        <span></span>
+                        <button className={cx('search-btn')}>
+                            <i className={cx('fa-solid fa-magnifying-glass')}></i>
+                        </button>
+                    </div>
+                </Tippy>
+                <div className={cx('action')}>
+                    <Button text>
+                        <i style={{ margin: '0px 6px' }} class="fa-solid fa-plus"></i>
+                        <span>Upload</span>
+                    </Button>
+                    <Button primary>
+                        <span>Log in</span>
+                    </Button>
                 </div>
-                <div className={cx('action')}>action</div>
             </div>
         </header>
     );
